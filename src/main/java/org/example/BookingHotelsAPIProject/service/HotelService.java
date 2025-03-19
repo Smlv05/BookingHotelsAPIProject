3c36c3c912123316cc38c1c174acd0ce6a87be73
Smlv05 <ersultan270502@gmail.com>
2025-03-19 21:12:21 +0100

    Creating HotelService

---
+++ src/main/java/org/example/BookingHotelsAPIProject/service/HotelService.java
@@ -0,0 +1,31 @@
+package org.example.BookingHotelsAPIProject.service;
+
+import org.example.BookingHotelsAPIProject.model.Hotel;
+import org.example.BookingHotelsAPIProject.repository.HotelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class HotelService {
+
+    private final HotelRepository hotelRepository;
+
+    public HotelService(HotelRepository hotelRepository) {
+        this.hotelRepository = hotelRepository;
+    }
+
+    public List<Hotel> getAllHotels() {
+        return hotelRepository.findAll();
+    }
+
+    public Optional<Hotel> getHotelById(Long id) {
+        return hotelRepository.findById(id);
+    }
+
+    public Hotel saveHotel(Hotel hotel) {
+        return hotelRepository.save(hotel);
+    }
+}
