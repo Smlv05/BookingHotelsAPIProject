dc102190e9bfb99e10e90a9e587a0649d57bc107
Smlv05 <ersultan270502@gmail.com>
2025-03-19 20:35:59 +0100

    Creating Hotel Class

---
+++ src/main/java/org/example/BookingHotelsAPIProject/model/Hotel.java
@@ -0,0 +1,22 @@
+package org.example.BookingHotelsAPIProject.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Hotel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String location;
+    private String address;
+    private double pricePerNight;
+    private int rating;
+}
